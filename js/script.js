1b9600d79df77ba082f9e9a9915c3a3b69411a5e
Adelak002 <olalekansaheed1234@gmail.com>
2023-05-01 07:50:04 -0700

    my like changes

--- js/script.js
+++ js/script.js
@@ -17,56 +17,57 @@ load()
 
 let row = document.getElementById('row')
 
+
 function showmovie(movies) {
-  
+
+  j = 0; 
   for (let i = 0; i < movies.length; i++) {
 
     let div = document.createElement('div')
     let path = "https://image.tmdb.org/t/p/original";
     let image = movies[i].backdrop_path;
     let title = movies[i].original_title;
-     
+     console.log(image);
 
     if (movies[i].original_title != undefined
       ) { 
         
-        div.classList.add('column');
-
         
-        div.innerHTML = `     
-      <img src="${path}${image}" alt="" class="imge">
-      <div class="hide" >
-          <div class="icons">
-              <div class="part1">
-          <button class="play"> <i class="fa-solid fa-play"></i></button>
-          <button class="add"><i class="fa-solid fa-plus"></i></button>
-          <button class="like"><i class="fa-solid fa-thumbs-up"></i></button>
-      </div>
-      <div class="part2">
-          <button class="more_info">
-              <i class="fa-solid fa-chevron-down"></i> 
-          </button> 
-      </div>
-      </div>
-  <div class="title">
-  <h3>${title.substr(0,14)} ${title.length >= 14 ?'...':''} </h3>
-  </div>
-  <div class="footer">  
-      <ul>
-          <li>Offbeat</li>
-          <li>Romamtic</li>
-          <li>Musical</li>
-      </ul>
-  </div>
-  
-  
-      </div>`
-    
+        div.classList.add('column');
+        
+        div.innerHTML = `
+        <img src=" ${path}${image}" alt="">
+
+<div class="centered">${title}</div>
+        
+<h3 class="title"> ${title}</h3>
+<div class="icons">
+    <i class="fa-regular like fa-thumbs-up"></i>
+    <i class="fa fa-share-nodes"></i>
+
+</div>
+      `
       row.append(div);
+
+let like_movie = document.querySelectorAll('.like');
+
+like_movie[j].onclick = function (){
+  this.classList.toggle("fa-thumbs-up")
+  this.classList.toggle("fa-thumbs-down")
+  
+}
+
+
+
+    j++;
     }
     
-  }
+  } 
 }
+
+
+
+
 
 
 
